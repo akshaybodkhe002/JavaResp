ca289332a6e8eef5b55d9b019011bf67eb20adfa
Akshay Sanjay Bodkhe <abodkhe011@gmail.com>
2024-10-19 16:35:09 +0530

    CxREX-3892 modified Hello.java and del hello.class

--- Hello.java
+++ Hello.java
@@ -2,4 +2,7 @@ public class Hello{
     public static void main(String[] args) {
         System.out.println("Hello java...");
     }
+    public void funt(){
+        System.out.println("This is time pass funtions...");
+    }
 }
